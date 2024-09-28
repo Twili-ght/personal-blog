826839929deebb17bb1b9988bd5a860c6900ce14
Twili-ght <2275066931@qq.com>
2024-09-28 15:50:32 +0800

    feat: 添加logo图标路径

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -9,7 +9,7 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ['meta', { name: 'msapplication-TileImage', content: '/personal-blog/logo.png' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
-  ['link', { rel: 'apple-touch-icon', href: '/personal-blog/logo.png' }],
+  ['link', { rel: 'apple-touch-icon', href: '/personal-blog/docs/public/favicon.ico' }],
   ['link', { rel: 'mask-icon', href: '/personal-blog/docs/public/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/personal-blog/manifest.webmanifest' }],
   [
