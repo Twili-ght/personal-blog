4a2a1364eccf3554cc1c40a3308ffb046a42be88
Twili-ght <2275066931@qq.com>
2024-09-29 00:11:11 +0800

    feat: 添加 Vue3 社区网站

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -10,7 +10,7 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'msapplication-TileImage', content: '/personal-blog/logo.png' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
   ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'mask-icon', href: '/personal-blog/favicon.ico', color: '#3eaf7c' }],
+  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/personal-blog/manifest.webmanifest' }],
   [
     'script',
