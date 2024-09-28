7ef9460d0a0267b141a247b7b4d886e92fa1a1e7
Twili-ght <2275066931@qq.com>
2024-09-28 14:55:22 +0800

    feat: 添加logo图标路径

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -7,10 +7,10 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/logo.png' }],
+  ['meta', { name: 'msapplication-TileImage', content: '/personal-blog/logo.png' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
-  ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
-  ['link', { rel: 'mask-icon', href: '/logo.png', color: '#3eaf7c' }],
+  ['link', { rel: 'apple-touch-icon', href: '/personal-blog/logo.png' }],
+  ['link', { rel: 'mask-icon', href: '/personal-blog/logo.png', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
   [
     'script',
